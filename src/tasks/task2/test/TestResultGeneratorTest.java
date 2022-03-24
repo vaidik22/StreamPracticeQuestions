8d210df490316295f7789c815049037b6350f8af
vaidik22 <vaidik.nigam_cs19@gla.ac.in>
2022-03-25 00:11:37 +0530

    we have created all methods and done all function of task 2

--- src/tasks/task2/test/TestResultGeneratorTest.java
+++ src/tasks/task2/test/TestResultGeneratorTest.java
@@ -1,13 +1,18 @@
 package tasks.task2.test;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tasks.task2.code.TestResultGenerator;
+
+import java.util.HashSet;
 
 class TestResultGeneratorTest {
-
+    private TestResultGenerator creator;
     @BeforeEach
     void setUp() {
+        creator = new TestResultGenerator();
     }
 
     @AfterEach
@@ -15,10 +20,13 @@ class TestResultGeneratorTest {
     }
 
     @Test
-    void getDetailsOfFailedStudents() {
+    short getDetailsOfFailedStudents() {
+        Assertions.assertEquals(getDetailsOfFailedStudents(), creator.getDetailsOfFailedStudents(new HashSet<>()));
+        return 0;
     }
 
     @Test
-    void sortStudentsForRanking() {
+    short sortStudentsForRanking() {
+        return 0;
     }
 }
