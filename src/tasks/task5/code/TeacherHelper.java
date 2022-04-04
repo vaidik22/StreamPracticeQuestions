ba4ea750f0ec8711c5e20e30de1e8a7b458caf41
vaidik22 <vaidik.nigam_cs19@gla.ac.in>
2022-04-04 22:52:59 +0530

    teacher helper class created

--- src/tasks/task5/code/TeacherHelper.java
+++ src/tasks/task5/code/TeacherHelper.java
@@ -2,13 +2,22 @@ package tasks.task5.code;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class TeacherHelper {
     public List<Teacher> findSubjectTeacher(List<Teacher> allTeachers, String subjectName) {
-        return List.of();
+        return allTeachers.stream()
+                .filter(e -> e.getSubjects().contains(subjectName))
+                .toList();
     }
 
+
     public Set<Teacher> getBusyTeachers(List<Teacher> allTeachers) {
-        return Set.of();
+        Predicate<Teacher> predicate = teacher -> teacher.getSubjects().size() >= 3;
+        return allTeachers.stream().filter(predicate)
+                .peek(e -> e.setTeacherSalary(e.getTeacherSalary() + e.getTeacherSalary() * 0.05))
+                .collect(Collectors.toSet());
     }
 }
+
